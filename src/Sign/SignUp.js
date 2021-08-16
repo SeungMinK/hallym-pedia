b8aa5335d719b000e949c9c5cc25f0773ee43fe5
SeungMinKim <ksm8797@naver.com>
2021-08-16 22:18:10 +0900

    [add file]회원가입 페이지 1차

---
+++ src/Sign/SignUp.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom"; //useHistroy를 훅이라고 부름
+import axios from "axios";
+
+function Login() {
+  return (
+    <>
+      <form>
+        <h3>Register</h3>
+
+        <div className="form-group">
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Last name</label>
+          <input type="text" className="form-control" placeholder="Last name" />
+        </div>
+
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-dark btn-lg btn-block">
+          Register
+        </button>
+        <p className="forgot-password text-right">
+          Already registered <a href="#">log in?</a>
+        </p>
+      </form>
+    </>
+  );
+}
+
+export default Login;
