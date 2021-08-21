637ef3d804e8d313b627a99096073de5cb50557b
SeungMinKim <ksm8797@naver.com>
2021-08-21 12:35:40 +0900

    [update] 회원가입 데이터 state로 저장 및 서버로 보낼 준비 완료

--- src/Sign/SignUp.js
+++ src/Sign/SignUp.js
@@ -3,23 +3,65 @@ import { useHistory, useParams } from "react-router-dom"; //useHistroy를 훅이
 import axios from "axios";
 
 function SignUp() {
+  const [Name, setName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  //typing 하면 onChange 를 발생시켜 state를 바꿔주어 value를 바꾼다.
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event) => {
+    // page refresh를 막아준다
+    event.preventDefault();
+
+    // 서버에 보내기
+    let body = {
+      name: Name,
+      email: Email,
+      password: Password,
+    };
+    console.log(
+      `submit 눌렷음 전송될 name : ${body.name} id : ${body.email} password : ${body.password}`
+    );
+    /*
+    dispatch(loginUser(body))
+      // 로그인되면 /(index페이지)로 이동
+      .then((response) => {
+        if (response.payload.loginSuccess) {
+          props.history.push("/");
+        } else {
+          alert("Error");
+        }
+      });
+    */
+    // axios.post('/api/users/login', body)
+    // .then(response => {
+
+    // })
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={onSubmitHandler}>
         <h3>Register</h3>
 
         <div className="form-group">
-          <label>First name</label>
+          <label>Name</label>
           <input
             type="text"
             className="form-control"
-            placeholder="First name"
+            placeholder="name"
+            value={Name}
+            onChange={onNameHandler}
           />
-        </div>
-
-        <div className="form-group">
-          <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
         </div>
 
         <div className="form-group">
@@ -28,6 +70,8 @@ function SignUp() {
             type="email"
             className="form-control"
             placeholder="Enter email"
+            value={Email}
+            onChange={onEmailHandler}
           />
         </div>
 
@@ -37,15 +81,14 @@ function SignUp() {
             type="password"
             className="form-control"
             placeholder="Enter password"
+            value={Password}
+            onChange={onPasswordHandler}
           />
         </div>
 
         <button type="submit" className="btn btn-dark btn-lg btn-block">
           Register
         </button>
-        <p className="forgot-password text-right">
-          Already registered <a href="#">log in?</a>
-        </p>
       </form>
     </>
   );
