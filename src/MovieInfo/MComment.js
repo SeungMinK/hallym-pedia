3ac5433c235fa11590cb59c98042349e16934335
0619jua <0619jua@naver.com>
2021-08-24 17:05:18 +0900

    [Add] 영화 댓글 달기 (미완료)

--- src/MovieInfo/MComment.js
+++ src/MovieInfo/MComment.js
@@ -1,49 +1,47 @@
-import { tsConstructorType } from '@babel/types';
-import { render } from '@testing-library/react';
-import React from 'react';
+// import React from 'react';
 
-class MComment extends React.Component {
-    render(){
-        constructor() {
-            super();
-            this.state = {
-                newReply : "",
-                replies : [{text : ""}]
-            };
-        }
+// class MComment extends React.Component {
+//     render(){
+//         constructor(); {
+//             super();
+//             this.state = {
+//                 newReply : "",
+//                 replies : [{text : ""}]
+//             };
+//         }
 
-        textChange = (3) => {
-            this.setState({ newReply : e.target.value });
-        }
+//         textChange = (e) => {
+//             this.setState({ newReply : e.target.value });
+//         }
 
-        buttonClick = () =>{
-            let arr = this.state.replies;
-            arr = arr.concat({ text : this.state.newReply});
+//         buttonClick = () =>{
+//             let arr = this.state.replies;
+//             arr = arr.concat({ text : this.state.newReply});
 
-            this.setState({ newReply : "" , replies : arr});
-        }
+//             this.setState({ newReply : "" , replies : arr});
+//         }
 
-        enterClick (e) = >{
-            if(e.key ==="Enter"){
-                this.buttonClick;
-                e.target.value = "";
-            };
-            
-        }
-    }
-    return (
-        <>
-            <ul className = "textbox"> {this.state.replies.map( (e) => (<li>e.text</li>)) }
-            </ul>
-            <textarea
-                className = "comment_textarea"
-                placeholder = "댓글 달기"
-                onChange = {this.textChange}
-                onKeyPress = {this.enterClick}>
-            </textarea>
-            <button className = "comment_button" onClick = {this.buttonClick}>게시</button>
-        </>
-    );
-}
+//         enterClick = (e) => {
+//             if(e.key ==="Enter"){
+//                 this.buttonClick;
+//                 e.target.value = "";
+//             };   
+//         }
 
-export default MComment;
+//         return (
+//             <>
+//                 <ul className = "textbox"> {this.state.replies.map( (e) => (<li>e.text</li>)) }
+//                 </ul>
+//                 <textarea
+//                     className = "comment_textarea"
+//                     placeholder = "댓글 달기"
+//                     onChange = {textChange}
+//                     onKeyPress = {enterClick}>
+//                 </textarea>
+//                 <button className = "comment_button" onClick = {this.buttonClick}>게시</button>
+//             </>
+//         );
+//     }
+// }
+
+// export default MComment;
