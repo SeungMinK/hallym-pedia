65e85fad9856ab4d3c9ad0c4d25c7c4869e4b9dd
0619jua <0619jua@naver.com>
2021-08-25 20:26:58 +0900

    [Update] moviesearch.jss - css 전체 수정

--- src/MovieSearch/MovieSearch.js
+++ src/MovieSearch/MovieSearch.js
@@ -55,16 +55,166 @@ const MovieSearch = () => {
             여름날 우리<p>2015 · 한국</p>
             <p>영화</p>
           </div>
+
+          <div id="img">
+            <img
+              src={process.env.PUBLIC_URL + `/movieImg/${0}/${0}_${5}.png`}
+              alt="copy url"
+            />
+            여름날 우리<p>2015 · 한국</p>
+            <p>영화</p>
+          </div>
+
+          <div id="img">
+            <img
+              src={process.env.PUBLIC_URL + `/movieImg/${0}/${0}_${5}.png`}
+              alt="copy url"
+            />
+            여름날 우리<p>2015 · 한국</p>
+            <p>영화</p>
+          </div>
+
+          <div id="img">
+            <img
+              src={process.env.PUBLIC_URL + `/movieImg/${0}/${0}_${5}.png`}
+              alt="copy url"
+            />
+            여름날 우리<p>2015 · 한국</p>
+            <p>영화</p>
+          </div> 
         </div>
       </div>
       <hr />
-      <div className="MovieSearch_box2">영화</div>
+
+      {/* 영화 박스 */}
+      <div className="MovieSearch_box2">
+        <div className="movie_search_title">
+          영화
+        </div>
+
+        <ul>
+          <li>
+            <div className = "movie_search_movie">
+              <div id="img">
+                <img
+                  src={process.env.PUBLIC_URL + `/movieImg/${0}/${0}_${5}.png`}
+                  alt="copy url"
+                />
+              </div>
+              <div id = "m_i">
+                <ul>
+                  <li>여름날 우리</li>
+                  <li>2015 한국</li>
+                </ul>  
+                <hr />
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className = "movie_search_movie">
+              <div id="img">
+                <img
+                  src={process.env.PUBLIC_URL + `/movieImg/${0}/${0}_${5}.png`}
+                  alt="copy url"
+                />
+              </div>
+              <div id = "m_i">
+                <ul>
+                  <li>여름날 우리</li>
+                  <li>2015 한국</li>
+                </ul>  
+                <hr />
+              </div>
+            </div>
+          </li>
+          
+          <li>
+            <div className = "movie_search_movie">
+              <div id="img">
+                <img
+                  src={process.env.PUBLIC_URL + `/movieImg/${0}/${0}_${5}.png`}
+                  alt="copy url"
+                />
+              </div>
+              <div id = "m_i">
+                <ul>
+                  <li>여름날 우리</li>
+                  <li>2015 한국</li>
+                </ul>  
+                <hr />
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className = "movie_search_movie">
+              <div id="img">
+                <img
+                  src={process.env.PUBLIC_URL + `/movieImg/${0}/${0}_${5}.png`}
+                  alt="copy url"
+                />
+              </div>
+              <div id = "m_i">
+                <ul>
+                  <li>여름날 우리</li>
+                  <li>2015 한국</li>
+                </ul>  
+                <hr />
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className = "movie_search_movie">
+              <div id="img">
+                <img
+                  src={process.env.PUBLIC_URL + `/movieImg/${0}/${0}_${5}.png`}
+                  alt="copy url"
+                />
+              </div>
+              <div id = "m_i">
+                <ul>
+                  <li>여름날 우리</li>
+                  <li>2015 한국</li>
+                </ul>  
+                <hr />
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
       <hr />
-      <div className="MovieSearch_box3">TV 프로그램</div>
+
+
+      {/* TV프로그램 박스 */}
+      <div className="MovieSearch_box3">
+        <div className="movie_search_title">TV 프로그램</div>
+        <div className = "movie_search_TV">
+          
+        </div>
+      </div>
       <hr />
-      <div className="MovieSearch_box4">책</div>
+
+
+      {/* 책 박스 */}
+      <div className="MovieSearch_box4">
+        <div className="movie_search_title">책</div>
+        <div className = "movie_search_book">
+          
+        </div>
+      </div>
       <hr />
-      <div className="MovieSearch_box5">사용자</div>{" "}
+
+
+      {/* 사용자 박스 */}
+      <div className="MovieSearch_box5">
+        <div className="movie_search_title">사용자</div>
+        <div className = "movie_search_user">
+          
+        </div>
+      </div>
+      {" "}
     </>
   );
 };
